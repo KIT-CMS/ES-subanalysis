9efd9fa528da5affd98c7097533ebd1f7d6a09dc
Olena Hlushchenko <greyxray@gmail.com>
2018-10-14 19:10:09 +0200

    shapes: change imports

--- shapes/__init__.py
+++ shapes/__init__.py
@@ -1,2 +1,6 @@
-from .shapes import Shapes
-from .etau_fes.etau_fes import ETauFES
+# -*- coding: utf-8 -*-
+
+__all__ = ['Shapes', 'ETauFES']
+
+from shapes.baseshapes import Shapes
+from etau_fes.etau_fes import ETauFES
--- shapes/etau_fes/__init__.py
+++ shapes/etau_fes/__init__.py
@@ -1,1 +1,6 @@
+# -*- coding: utf-8 -*-
+
+__all__ = ['Shapes', 'ETauFES']
+
+from shapes import Shapes
 from .etau_fes import ETauFES
