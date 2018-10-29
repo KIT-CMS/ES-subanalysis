1ea84dee4c1384abfd4704706694ffc3de5b2a53
Olena Hlushchenko <greyxray@gmail.com>
2018-10-29 06:40:22 +0100

    utils: tune printout

--- utils/produce_shapes_variables.py
+++ utils/produce_shapes_variables.py
@@ -6,7 +6,7 @@ from shapes.etau_fes import etau_fes
 from shape_producer.systematics import Systematics
 
 
-def prepareConfig(config_file='data/et_fes_config.yaml'):
+def prepareConfig(config_file='data/et_fes_config.yaml', debug=False):
     '''Read config and update to prompt'''
     config = etau_fes.ETauFES.readConfig(config_file)
 
@@ -14,32 +14,37 @@ def prepareConfig(config_file='data/et_fes_config.yaml'):
 
     config.update(prompt_args)
 
+    if debug:
+        print 'config:'
+        pp.pprint(config)
+
     return config
 
 
 def main():
     print 'Start'
 
-    print '# 1 - prepareConfig'
-    config = prepareConfig()
+    print '\n# 1 - prepareConfig'
+    config = prepareConfig(debug=False)
 
-    print '# 2 - setup_logging'
+    print '\n# 2 - setup_logging'
     shapes = etau_fes.ETauFES(**config)
     shapes.setup_logging(output_file="{}_etau_fes.log".format(shapes._tag), level=logging.INFO, logger=shapes._logger)
 
-    print '# 3 - Era evaluation'
+    print '\n# 3 - Era evaluation'
     shapes.evaluateEra()
-    shapes._nominal_folder = 'eleTauEsOneProngPiZerosShift_0'
-    print '# 4 - import necessary estimation methods'
+    # shapes._nominal_folder = 'eleTauEsOneProngPiZerosShift_0'
+    print '\n# 4 - import necessary estimation methods'
     shapes.importEstimationMethods()
 
-    print '# 5 - evaluating channels (processes, variables,cattegories)'
+    print '\n# 5 - evaluating channels (processes, variables,cattegories)'
     shapes.evaluateChannels()
 
-    print '# 6 - add systematics'
+    print '\n# 6 - add systematics'
     shapes.evaluateSystematics(
         'nominal',
         'TES',
+        'FES_shifts',
     )
 
     print '# 7 - produce shapes'
