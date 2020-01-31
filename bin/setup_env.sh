505ab3fe4c5dd1774ab171f32c415b3b11a0a06e
Olena Hlushchenko <greyxray@gmail.com>
2020-01-31 20:04:12 +0100

    bash helpers

--- bin/setup_env.sh
+++ bin/setup_env.sh
@@ -6,3 +6,5 @@ EXECFROM_DIR="$( dirname "${BASH_SOURCE[0]}" )"
 source $EXECFROM_DIR/setup_cvmfs_sft.sh
 
 source $EXECFROM_DIR/setup_python.sh
+
+source $EXECFROM_DIR/setup_helpers.sh
---
+++ bin/setup_helpers.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+prep_resubmit_silent() {
+    if [[ $# -eq 0 ]] ; then
+        arguments=$(pwd)/arguments.txt
+    elif [[ $# -eq 1 ]] ; then
+        if [ -f "$1" ]; then
+            # echo "$1 exist"
+            arguments=$1
+        elif [ -f "$1/arguments.txt" ]; then
+            arguments=$1
+        else
+            echo "$1[/arguments.txt] is not a valid file"
+            return
+        fi
+    fi
+
+    # echo arguments: $arguments
+    dirpath=$(dirname "$arguments")
+    f="$(basename -- $arguments)"
+    f="${f%.*}"
+    arguments_finished=${dirpath}/${f}_finished.txt
+    mv ${arguments} ${arguments_finished}
+
+    # arguments_resubmit=${dirpath}/${f}_resubmit.txt
+    # echo arguments: $arguments
+    # echo arguments: $arguments
+    ERRORED_MYID=()
+    ERRORED_CONDORID=()
+    touch ${arguments}
+    # success_dir=${dirpath}/out/success
+    # fail_dir=${dirpath}/out/prepared_to_resubmit
+    # if [[ ! -d "${fail_dir}" ]]
+    # then
+    #     mkdir -p ${fail_dir}
+    # fi
+    # if [[ ! -d "${success_dir}" ]]
+    # then
+    #     mkdir -p ${success_dir}
+    # fi
+    for fullfile in ${dirpath}/out/*
+    do
+        filename="${fullfile##*/}"
+        extension="${filename##*.}"
+        condorid="${filename#*.}"
+        condorid="${condorid%.*}"
+        jobid="${filename%%.*}"
+        if [[ ! $(tail -n 1 $fullfile) == "done"* ]]
+        then
+            ERRORED_MYID+=($jobid)
+            ERRORED_CONDORID+=($condorid)
+            # mv $fullfile $fail_dir/$fullfile
+        # else
+        #     mv $fullfile $success_dir/$fullfile
+        fi
+    done
+    while IFS= read -r line
+    do
+        for jobid in "${ERRORED_MYID[@]}"
+        do
+            if [[ ${line} == ${jobid}* ]] ; then
+                echo "$line" >> ${arguments}
+            fi
+        done
+    done < ${arguments_finished}
+
+    echo $arguments
+}
+
+prep_resubmit() {
+    arguments=$(prep_resubmit_silent $@)
+    echo "Number to resubmit:"
+    cat ${arguments} | wc -l
+    echo "$arguments"
+}
+# prep_resubmit
+
+submit_prep_resubmit() {
+    arguments=$(prep_resubmit_silent $@)
+    echo "Number to resubmit:"
+    cat ${arguments} | wc -l
+    echo "$arguments"
+    dirpath=$(dirname "$arguments")
+    # cd $dirpath && condor_submit job.jdl
+    # cd -
+}
+# submit_prep_resubmit
